0b08ae8f9be09f4e8cf58086405d39263ad859ae
EsZe <33262870+eszepto@users.noreply.github.com>
2020-09-18 22:47:59 +0700

    finish project 4.2 4.3.1 4.3.3 4.4 4.5

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController 
     def index
-        @movies = Movie.all
+        @movies = Movie.sort_by_title
     end
 
     def show
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,5 @@
 class Movie < ActiveRecord::Base
-    
+    def self.sort_by_title
+        return self.all.order(:title)
+    end
 end
