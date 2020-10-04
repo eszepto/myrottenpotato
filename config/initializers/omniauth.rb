c251114e187e5a7c066e98ce7483738a608f4330
Pichet <bestoinw@hotmail.com>
2020-10-04 20:04:16 +0700

    use dot-env to store twitter api key

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+# Replace API_KEY and API_SECRET with the values you got from Twitter
+Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  end
